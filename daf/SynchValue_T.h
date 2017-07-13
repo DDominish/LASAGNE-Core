b190afea312373dae7f359e2f784ace7dcfbc0ed
Derek Dominish <derek.dominish@dsto.defence.gov.au>
2017-07-13 13:10:20 +0930

    Complete refactor to handle propper thread unwinding and removed previous cludge for shutting down. Have to now handle InterruptedException properly in tests.

--- daf/SynchValue_T.h
+++ daf/SynchValue_T.h
@@ -1,63 +1,31 @@
 /***************************************************************
-    Copyright 2016, 2017 Defence Science and Technology Group,
-    Department of Defence,
-    Australian Government
+Copyright 2016, 2017 Defence Science and Technology Group,
+Department of Defence,
+Australian Government
 
-	This file is part of LASAGNE.
+This file is part of LASAGNE.
 
-    LASAGNE is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as
-    published by the Free Software Foundation, either version 3
-    of the License, or (at your option) any later version.
+LASAGNE is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3
+of the License, or (at your option) any later version.
 
-    LASAGNE is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
+LASAGNE is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public
-    License along with LASAGNE.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Lesser General Public
+License along with LASAGNE.  If not, see <http://www.gnu.org/licenses/>.
 ***************************************************************/
 #ifndef DAF_SYNCHVALUE_T_H
 #define DAF_SYNCHVALUE_T_H
-
-/**
-* @file     SynchValue_T.h
-* @author   Derek Dominish
-* @author   $LastChangedBy$
-* @date     1st September 2011
-* @version  $Revision$
-* @ingroup
-* @namespace DAF
-*/
 
 #include "Monitor.h"
 #include "Semaphore.h"
 
 namespace DAF
 {
-     /** @class SynchWaiterSemaphore
-     * @brief Extension of DAF::Semaphore to allow a direct acquisition of a number
-     * of permits.
-     *
-     * Details \todo{Detailed description}
-     */
-    class SynchWaiterSemaphore : public DAF::Semaphore
-    {
-    public:
-        SynchWaiterSemaphore(int permits = 0) : DAF::Semaphore(permits)
-        {}
-
-        /// Acquire n times.
-        virtual int acquire(int permits)
-        {
-            while(permits-- >= 0 ) {
-                Semaphore::acquire();
-            }
-            return 0;
-        }
-    };
-
      /** @class SynchValue
      * @brief Synchronize on a particular value.
      *
@@ -67,44 +35,27 @@ namespace DAF
      * When a the requested value is found. the Waiting Thread will
      * exit and return.
      */
-    template <typename T = bool>
-    class SynchValue : protected DAF::Monitor
+    template <typename T>
+    class SynchValue_T : protected Monitor
     {
-
-        /// current value held.
-        volatile T value_;
-
-        /// flag to indicate Object is destroying.
-        volatile bool shutdown_;
-
-        /// flag for number of waiters
-        volatile size_t   waiters_;
-
-        /// Setter semaphore
-        SynchWaiterSemaphore    itemTaken_;
-        DAF::Semaphore          setterGate_;
+        using Monitor::wait;    // Hide Monitor::wait
 
     public:
 
-        /** \todo{Fill this in} */
-        typedef T _value_type;
+        typedef T   _value_type;
 
+        typedef typename Monitor::_mutex_type   _mutex_type;
 
-        /**
-         *  \todo{Fill this in}
-         */
-        SynchValue(const T &value = T());
+        SynchValue_T(const T & value = T());
 
-        /// Upon destruction this value will notifyAll waiters and will cause them to throw IllegalThreadStateException
-        virtual ~SynchValue();
+        virtual ~SynchValue_T(void);
 
-        /**
-         * getValue
-         *
-         * Get the current value of the SynchValue.
-         * @return current value of the SynchValue.
-         */
-        virtual T   getValue(void) const;
+        using Monitor::waiters;
+        using Monitor::interrupt;
+        using Monitor::interrupted;
+
+        /// Return the current value
+        T   getValue(void) const;
 
         /**
          * setValue
@@ -113,9 +64,10 @@ namespace DAF
          * notifyAll is used on all waiters. It will not exit until all
          * waiters have checked the current value.
          *
-         * @return 0 upon success. -1 upon failure to acquire guard.
+         * @return 0 upon success.
+         *         -1 if other error (Use DAF_OS::last_error() to examine error).
          */
-        virtual int setValue(const T &value);
+        int setValue(const T & value);
 
         /**
          * waitValue
@@ -126,16 +78,75 @@ namespace DAF
          *
          * @arg value - The value to wait on.
          *
-         * @return 0 upon completion when value has been set.
-         *         -1 if the guard could not be acquired.
-         *
-         * @throw IllegalThreadStateException upon the SynchValue being destroyed while waiting
+         * @return 0  upon completion when value has been set.
+         * throws DAF::TimeoutException on timeout
+         * otherwise -1 if other error (Use DAF_OS::last_error() to examine error).
          */
-        virtual int waitValue(const T &value);
+        int waitValue(const T & value, const ACE_Time_Value * abstime = 0) const;
 
-        virtual int waitValue(const T &value, time_t msec);
+        /** Wait on a Latched Value until abs time value tv */
+        int waitValue(const T & value, const ACE_Time_Value & abstime) const;
 
+        /** Wait on a Latched Value for upto msec */
+        int waitValue(const T & value, time_t msec) const;
+
+    protected:
+
+        T  value_;
+
+    private:
+
+        mutable _mutex_type valueLock_;
+        mutable class ValueSemaphore : public Semaphore
+        {
+        public:
+
+            ValueSemaphore(int permits = 1);
+            ~ValueSemaphore(void);
+
+            int releaseWaiter(void);
+            int acquireWaiter(void);
+
+        private:
+
+            class ValueWaiters : Monitor
+            {
+                int valueWaiters_;
+
+            public:
+
+                ValueWaiters(int valueWaiters = 0);
+                ~ValueWaiters(void);
+
+                int operator ++ (); // Prefix
+                int operator -- (); // Prefix
+
+            } valueWaiters_;
+
+        } valueSemaphore_;
+
+        class ValueSemaphoreWaiterGuard
+        {
+            ValueSemaphore & value_semaphore_;
+
+        public:
+
+            ValueSemaphoreWaiterGuard(ValueSemaphore & value_semaphore) : value_semaphore_(value_semaphore) {
+                this->value_semaphore_.acquireWaiter();
+            }
+            ~ValueSemaphoreWaiterGuard(void) {
+                this->value_semaphore_.releaseWaiter();
+            }
+        };
     };
+
+    template <typename T>
+    struct SynchValue : SynchValue_T<T> {   // Backwards Compatability
+        SynchValue(const T & value = T()) : SynchValue_T<T>(value) {}
+    };
+
+    typedef SynchValue<bool>    SynchLatch; // Backwards Compatability
+
 }  // namespace DAF
 
 #if defined (ACE_TEMPLATES_REQUIRE_SOURCE)
