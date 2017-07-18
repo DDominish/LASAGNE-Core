45dfc374080b8b48c3b1fc9dcbaa7c4039549369
Derek Dominish <derek.dominish@dsto.defence.gov.au>
2017-07-18 10:40:00 +0930

    Initial refactor - Not Complete

--- daf/Rendezvous_T.cpp
+++ daf/Rendezvous_T.cpp
@@ -23,6 +23,8 @@
 
 #include "Rendezvous_T.h"
 
+#include <ace/Min_Max.h>
+
 namespace DAF
 {
     template <typename T> void
@@ -36,8 +38,8 @@ namespace DAF
     /********************************************************************/
 
     template <typename T, typename F>
-    Rendezvous<T,F>::Rendezvous(size_t parties,  F &fn)
-        : entryGate_    (int(parties))
+    Rendezvous<T,F>::Rendezvous(int parties)
+        : entryGate_    (parties)
         , parties_      (parties)
         , synch_        (0)
         , count_        (0)
@@ -45,195 +47,149 @@ namespace DAF
         , broken_       (false)
         , shutdown_     (false)
         , triggered_    (false)
-        , fn_           (fn)
     {
         if ( parties == 0 ) throw InitializationException("DAF::Rendezvous Initialization Error parties == 0");
 
         this->slots_.reserve(parties + 2);  // Reserve enough space for manipulation (+2)
     }
 
-    template <typename T, typename F>
-    Rendezvous<T,F>::~Rendezvous(void)
-    {
-        ACE_Guard<ACE_SYNCH_MUTEX> ace_mon( *this );
-        this->shutdown_ = true;  // signal we are shutting down
-
-        // clear out the entryGate_.waiters(). They should hit
-        // the shutdown flag and exit.
-        this->entryGate_.release(this->entryGate_.waiters() + 1);
-
-        while (this->slots_.size() && this->count_ != 0 ) {
-            // Having set the shutdown_ flag. All waiters should be notified.
-            // But we need to release the lock on this->wait(). One thing to consider
-            // is whether this->wait() will be reacquired prior to all threads have exit?
-            // The entrant threads need the object's state to complete.
-            // If destruction is found to be problematic consider refactoring
-            // the rendezvous entrant logic to push state on the stack prior to
-            // subsequent calls to notifyAll().
-            this->notifyAll();
-
-            this->wait(100);
-        }
-    }
 
     template <typename T, typename F> bool
-    Rendezvous<T,F>::waitReset(time_t msecs)
+    Rendezvous<T, F>::broken(void) const
     {
-        ACE_Guard<ACE_SYNCH_MUTEX> ace_mon( *this );
+        return this->broken_;
+    }
 
-        for (size_t synch = this->synch_; this->slots_.size();) try {
-            if (this->shutdown_) {
-                throw IllegalThreadStateException();
-            } else if (this->synch_ == synch) {
-                if (this->triggered_ || this->broken_) {
-                    this->notifyAll();
+    template <typename T, typename F> int
+    Rendezvous<T, F>::parties(void) const
+    {
+        return this->parties_;
+    }
+
+    template <typename T, typename F> T
+     Rendezvous<T, F>::rendezvous(const T & t, const ACE_Time_Value * abstime)
+    {
+        while (!this->interrupted()) {
+
+            ACE_GUARD_READTION(_mutex_type, mon, *this, DAF_THROW_EXCEPTION(LockFailureException));
+
+            if (this->entryGate_.acquire(abstime)) {
+                switch (this->interrupted() ? EINTR : DAF_OS::last_error()) {
+                case EINTR: continue;
+                case ETIME: DAF_THROW_EXCEPTION(TimeoutException);
+
+                default: return -1;
                 }
-                if (this->wait(DAF_OS::gettimeofday(msecs))) {
+            }
+
+            int index = this->count_++; this->slots_.push_back(t); this->resets_ = this->count_;
+
+            while (!(this->broken_ || this->triggered_)) try {
+                if (this->count_ == this->parties_) {
+                    F()(this->slots_); this->triggered_ = true; break;
+                }
+                else if (this->wait(abstime)) {
+                    switch (this->interrupted() ? EINTR : DAF_OS::last_error()) {
+                    case EINTR: DAF_THROW_EXCEPTION(InterruptedException);
+                    case ETIME: DAF_THROW_EXCEPTION(TimeoutException);
+                    default:    DAF_THROW_EXCEPTION(BrokenBarrierException);
+                    }
+                }
+            }
+            catch (...) {
+                ACE_Errno_Guard g(errno); ACE_UNUSED_ARG(g);
+                --this->count_; --this->resets_;
+                this->broken_ = true;
+                this->broadcast();
+                throw;
+            }
+
+        }
+
+        DAF_THROW_EXCEPTION(InterruptedException);
+    }
+    //    const ACE_Time_Value end_time(DAF_OS::gettimeofday(msec));
+
+    //    bool timeout    = false;
+    //    size_t index    = this->count_;
+
+    //    T t_rtn(t);
+
+    //    this->slots_.push_back(t);
+    //    this->resets_ = ++this->count_;
+
+    //    while (!(this->broken_ || this->triggered_)) try {
+    //        if (this->shutdown_) {
+    //            this->broken_ = true;
+    //        } else if (this->count_ != this->parties_) {
+    //            if (end_time > DAF_OS::gettimeofday()) {
+    //                this->wait(end_time);
+    //            } else timeout = this->broken_ = true;
+    //        } else {
+    //            F()(this->slots_); this->triggered_ = true;
+    //        }
+    //    } catch(...) { // JB: Deliberate catch(...) - DON'T replace with DAF_CATCH_ALL
+    //        --this->resets_;
+    //        --this->count_;
+    //        this->broken_ = true;
+    //        this->notifyAll();
+    //        throw;
+    //    }
+
+    //    if (index >= this->slots_.size()) {
+    //        this->broken_ = true;
+    //    } else {
+    //        t_rtn = this->slots_[index];
+    //    }
+
+    //    bool broken = this->broken_, shutdown = this->shutdown_; // Get state onto the stack
+
+    //    this->notifyAll();
+
+    //    if (--this->resets_ == 0) {
+    //        this->slots_.clear();
+    //        this->entryGate_.release(int(this->count_));
+    //        this->broken_ = this->triggered_ = false; ++this->synch_;
+    //        this->count_ = 0;
+    //    }
+
+    //    if (timeout) {
+    //        throw TimeoutException();
+    //    } else if (shutdown) {
+    //        throw IllegalThreadStateException();
+    //    } else if (broken) {
+    //        throw BrokenBarrierException();
+    //    }
+
+    //    return t_rtn;
+    //}
+
+    template <typename T, typename F> bool
+    Rendezvous<T, F>::waitReset(const ACE_Time_Value * abstime)
+    {
+        for (size_t synch = this->synch_; this->slots_.size();) try {
+
+            ACE_GUARD_READTION(_mutex_type, mon, *this, DAF_THROW_EXCEPTION(LockFailureException));
+
+
+            if (this->synch_ == synch) {
+                if (this->triggered_ || this->broken_) {
+                    this->broadcast();
+                }
+                else if (this->wait(abstime)) {
                     this->broken_ = true;
-                    this->notifyAll();
+                    this->broadcast();
                     return false;
                 }
             } else break;
-        } catch(...) { // JB: Deliberate catch(...) - DON'T replace with DAF_CATCH_ALL
-            this->broken_ = true;
-            this->notifyAll();
-            throw;
+
+        } catch (...) { // JB: Deliberate catch(...) - DON'T replace with DAF_CATCH_ALL
+            this->broken_ = true; this->broadcast(); throw;
         }
 
         return true;
     }
 
-    template <typename T, typename F> T
-    Rendezvous<T,F>::rendezvous(const T &t)
-    {
-        this->entryGate_.acquire();
-
-        if ( this->shutdown_ ) {
-            throw IllegalThreadStateException();
-        }
-
-        ACE_Guard<ACE_SYNCH_MUTEX> ace_mon( *this );
-
-        size_t index = this->count_;
-
-        T t_rtn(t);
-
-        this->slots_.push_back(t);
-
-        this->resets_ = ++this->count_;
-
-        while (!(this->broken_ || this->triggered_)) try {
-            if (this->shutdown_) {
-                this->broken_ = true;
-            } else if (this->count_ != this->parties_) {
-                this->wait();
-            } else  {
-                this->fn_(this->slots_);
-                this->triggered_ = true;
-            }
-        } catch(...) { // JB: Deliberate catch(...) - DON'T replace with DAF_CATCH_ALL
-            --this->resets_;
-            --this->count_;
-            this->broken_ = true;
-            this->notifyAll();
-            throw;
-        }
-
-        if (index >= this->slots_.size()) {
-            this->broken_ = true;
-        } else {
-            t_rtn = this->slots_[index];
-        }
-
-        bool broken = this->broken_, shutdown = this->shutdown_; // Get state onto the stack
-
-        this->notifyAll();
-
-        if (--this->resets_ == 0) {
-            this->slots_.clear();
-            this->entryGate_.release(int(this->count_));
-            this->broken_ = this->triggered_ = false; ++this->synch_;
-            this->count_ = 0;
-        }
-
-        if (shutdown) {
-            throw IllegalThreadStateException();
-        } else if (broken) {
-            throw BrokenBarrierException();
-        }
-
-        return t_rtn;
-    }
-
-    template <typename T, typename F> T
-    Rendezvous<T,F>::rendezvous(const T &t, time_t msec)
-    {
-        if (this->entryGate_.attempt(msec)) {
-            throw TimeoutException();
-        }
-
-        if ( this->shutdown_ ) {
-            throw IllegalThreadStateException();
-        }
-
-        ACE_Guard<ACE_SYNCH_MUTEX> ace_mon( *this );
-
-        const ACE_Time_Value end_time(DAF_OS::gettimeofday(msec));
-
-        bool timeout    = false;
-        size_t index    = this->count_;
-
-        T t_rtn(t);
-
-        this->slots_.push_back(t);
-        this->resets_ = ++this->count_;
-
-        while (!(this->broken_ || this->triggered_)) try {
-            if (this->shutdown_) {
-                this->broken_ = true;
-            } else if (this->count_ != this->parties_) {
-                if (end_time > DAF_OS::gettimeofday()) {
-                    this->wait(end_time);
-                } else timeout = this->broken_ = true;
-            } else {
-                this->fn_(this->slots_);
-                this->triggered_ = true;
-            }
-        } catch(...) { // JB: Deliberate catch(...) - DON'T replace with DAF_CATCH_ALL
-            --this->resets_;
-            --this->count_;
-            this->broken_ = true;
-            this->notifyAll();
-            throw;
-        }
-
-        if (index >= this->slots_.size()) {
-            this->broken_ = true;
-        } else {
-            t_rtn = this->slots_[index];
-        }
-
-        bool broken = this->broken_, shutdown = this->shutdown_; // Get state onto the stack
-
-        this->notifyAll();
-
-        if (--this->resets_ == 0) {
-            this->slots_.clear();
-            this->entryGate_.release(int(this->count_));
-            this->broken_ = this->triggered_ = false; ++this->synch_;
-            this->count_ = 0;
-        }
-
-        if (timeout) {
-            throw TimeoutException();
-        } else if (shutdown) {
-            throw IllegalThreadStateException();
-        } else if (broken) {
-            throw BrokenBarrierException();
-        }
-
-        return t_rtn;
-    }
 } // namespace DAF
 
 #endif  // DAF_RENDEZVOUS_T_CPP
